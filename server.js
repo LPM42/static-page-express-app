6fccbfda7337450c1b453b3c13eb33be2708ec3d
Livingston Martin <livingston@drmartin.com>
2014-08-09 13:52:33 -0700

    express app

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/static', function(request, response) {
+  res.sendFile(__dirname + '/static/index.html')
+});
+
+var server = http.createServer(app);
+
+server.listen(3000, function() {
+  console.log('Server running on port 3000.');
+});
+
